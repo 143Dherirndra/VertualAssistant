c9a11bc38fc3ae980a70c2dc013c3266b3d5a761
143Dherirndra <pal835809@gmail.com>
2025-12-30 20:52:56 +0530

    //I  forgate

--- Backend/middleware/multer.js
+++ Backend/middleware/multer.js
@@ -1,17 +1,4 @@
-// import multer from 'multer'
 
-
-// const  storage= multer.diskStorage({
-//     destination:(req,file,cb)=>{
-//         cb(null,"./public")
-//     },
-//     fileName:(req,file,cb)=>{
-//         cb(null,file.originalName)
-
-//     }
-// })
-// const upload = multer({storage})
-// export default upload;
 
 import multer from "multer";
 import path from "path";
