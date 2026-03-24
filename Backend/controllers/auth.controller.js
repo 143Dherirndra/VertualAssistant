311daab5fa3dfcd1024243248388d54513374475
143Dherirndra <pal835809@gmail.com>
2026-03-24 15:01:07 +0530

    Update cookie settings in auth controller

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -28,11 +28,11 @@ const user = await User.create({
 
        const token= await genToken(user._id)
 
-       res.cookie("token", token, {
+     res.cookie("token", token, {
   httpOnly: true,
-  secure: true,        
-  sameSite: "None",      
-  maxAge: 7 * 24 * 60 * 60 * 1000
+  secure: false,
+  sameSite: "lax", 
+  maxAge: 7 * 24 * 60 * 60 * 1000,
 });
 
        return res.status(200).json(user)
